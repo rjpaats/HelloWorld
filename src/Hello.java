4a5cf20cae9c3af060320052a5730078fb66dd1e
R.J. Paats <r.j.paats@paats.nl>
2018-07-01 18:09:27 +0200

    Initial commit

---
+++ src/Hello.java
@@ -0,0 +1,26 @@
+/**
+ * Created by Robert Paats 20 Mei 2018
+ */
+public class Hello {
+    public static void main(String[] args) {
+
+        System.out.println("Hello Robert!");
+
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+
+        int myVerschil = 1000 - myTotal;
+
+        System.out.println(myFirstNumber);
+        System.out.println("mySecondNumber");
+        System.out.println(myThirdNumber);
+
+        System.out.println(myTotal);
+
+        System.out.println(myVerschil);
+
+    }
+}
